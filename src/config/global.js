fc45719889966fd0eb9d61f7bc608219f6228578
jhonuru <47787645+jhonuru@users.noreply.github.com>
2025-07-16 01:04:48 -0500

    Ajustes

--- src/config/global.js
+++ src/config/global.js
@@ -155,7 +155,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/22050109_CF02_DU',
+        download: 'downloads/22050109_CF02_DU.zip',
       },
       {
         icono: 'fas fa-download',
